1d78ea3739896a0d69fc06acc3f481e5ed0a325f
basticks02 <ekomobongekanem10@gmail.com>
2025-02-24 15:13:27 -0500

    Updates

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar({ transparent }) {
         <li><a href="#experiences">Experiences</a></li>
         <li><a href="#timeline">Timeline</a></li>
         <li><a href="#projects">Projects & Others</a></li>
-        <li><a href="https://drive.google.com/file/d/177x4HKw1a3V8NOZwraeH-QpesKGgBr0i/view?usp=sharing">Resume</a></li>
+        <li><a href="https://drive.google.com/file/d/1PJhOzRDVBsFmksnsxsiFqHSbXkWk9c14/view?usp=sharing">Resume</a></li>
       </ul>
       
     </nav>
